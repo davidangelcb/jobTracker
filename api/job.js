171387a12a20caadf732463d7752179ced9850c4
David C <davidangelcb@gmail.com>
2025-07-16 12:26:07 -0500

    fix

--- api/job.js
+++ api/job.js
@@ -10,12 +10,12 @@ export default async function handler(req, res) {
     const jobId = req.query.id;
 
     // Supongamos que haces un fetch externo usando tu clave secreta
- 
+   //4d27ce6e-f83f-4d5b-9801-1cbfc8e53582'}
     const apiKey = process.env.API_KEY_PRIVADA;
-    const urlApi = " https://xnq1m085i6.execute-api.us-east-1.amazonaws.com/uat/api/job/4d27ce6e-f83f-4d5b-9801-1cbfc8e53582";
+    const urlApi = "https://xnq1m085i6.execute-api.us-east-1.amazonaws.com/uat/api/job/"+jobId;
 
-    if (req.method === 'AA') {
-      console.log(urlApi+jobId);
+    if (req.method === 'GET') {
+     
       const response = await fetch(urlApi, {
         headers: {
           "Content-Type": "application/json",
@@ -25,8 +25,7 @@ export default async function handler(req, res) {
   
       const data = await response.json();
       return res.status(200).json(data);
-    }
-    return res.status(200).json({ message: 'get correctamente:'+jobId, received: req.params });
+    }     
   }
 
   if (req.method === 'POST') {
